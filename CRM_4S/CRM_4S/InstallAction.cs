631aa79fae1ab84ac75742ad35b072e6efbbad06
zjk <zhangjie1@yicai.com>
2016-01-14 16:17:55 +0800

    add new demand

---
+++ CRM_4S/CRM_4S/InstallAction.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CRM_4S
+{
+    [RunInstaller(true)]
+    public partial class InstallAction : Installer
+    {
+        public InstallAction()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            try
+            {
+                string physicalRoot = this.Context.Parameters["targetdir"]; // 安装物理路径
+
+                try
+                {
+                    //由于执行Register.bat文件的时候，gacutil.exe会以System目录作为起始目录，
+                    //因此在Bat文件中设置一个参数，安装的时候替换为具体的目录
+                    string Register = Path.Combine(physicalRoot, "Register.bat");
+                    ReplaceName(Register, "{INSTALL_PATH}", physicalRoot);
+                }
+                catch { }
+
+                string registerFile = Path.Combine(physicalRoot, "Register.bat");
+                RunDos(registerFile, "", false);
+            }
+            catch (Exception ex)
+            {
+                //WriteLog(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 在运行脚本之前把脚本中的指定名称替换成新的名词
+        /// </summary>
+        /// <param name="filePath">脚本文件名</param>
+        /// <param name="oldDBName">原有的名称</param>
+        /// <param name="newDBName">新的名称</param>
+        private void ReplaceName(string filePath, string oldName, string newName)
+        {
+            if (newName.CompareTo(oldName) != 0)
+            {
+                string fileText = string.Empty;
+                using (StreamReader streamReader = new StreamReader(filePath, Encoding.Default))
+                {
+                    fileText = streamReader.ReadToEnd();
+                    fileText = fileText.Replace(oldName, newName);
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter(filePath, false, Encoding.Default))
+                {
+                    streamWriter.Write(fileText);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 后台执行DOS文件
+        /// </summary>
+        /// <param name="fileName">文件名(包含路径)</param>
+        /// <param name="argument">运行参数</param>
+        /// <param name="hidden">是否隐藏窗口</param>
+        private void RunDos(string fileName, string argument, bool hidden)
+        {
+            Process process = new Process();
+            process.EnableRaisingEvents = false;
+            process.StartInfo.FileName = string.Format("\"{0}\"", fileName);
+            process.StartInfo.Arguments = argument;
+            if (hidden)
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            }
+            else
+            {
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            }
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
---
+++ CRM_4S/CRM_4S/Register.bat
@@ -0,0 +1,20 @@
+﻿@echo off
+
+set path="{INSTALL_PATH}"
+
+gacutil -u "{INSTALL_PATH}\DevExpress.Data.v11.1"
+mkdir %windir%\assembly\GAC_MSIL\DevExpress.Data.v11.1\11.1.6.0__b88d1754d700e49a
+copy "{INSTALL_PATH}\DevExpress.Data.v11.1.dll" %windir%\assembly\GAC_MSIL\DevExpress.Data.v11.1\11.1.6.0__b88d1754d700e49a
+
+gacutil -u "{INSTALL_PATH}\DevExpress.Utils.v11.1"
+mkdir %windir%\assembly\GAC_MSIL\DevExpress.Utils.v11.1\11.1.6.0__b88d1754d700e49a
+copy "{INSTALL_PATH}\DevExpress.Utils.v11.1.dll" %windir%\assembly\GAC_MSIL\DevExpress.Utils.v11.1\11.1.6.0__b88d1754d700e49a
+
+gacutil -u "{INSTALL_PATH}\DevExpress.CodeRush.Common"
+mkdir %windir%\assembly\GAC_MSIL\DevExpress.CodeRush.Common\11.1.6.0__35c9f04b7764aa3d
+copy "{INSTALL_PATH}\DevExpress.CodeRush.Common.dll" %windir%\assembly\GAC_MSIL\DevExpress.CodeRush.Common\11.1.6.0__35c9f04b7764aa3d
+REM copy "{INSTALL_PATH}\DevExpress.CodeRush.Common.dll" "C:\Program Files\DevExpress 2011.1\IDETools\System\DXCore\BIN\DevExpress.CodeRush.Common.dll"
+if "[%ProgramFiles(x86)%]" == "[]" (copy "{INSTALL_PATH}\DevExpress.CodeRush.Common.dll" "%ProgramFiles%\DevExpress 2011.1\IDETools\System\DXCore\BIN\DevExpress.CodeRush.Common.dll") else (copy "{INSTALL_PATH}\DevExpress.CodeRush.Common.dll" "%ProgramFiles(x86)%\DevExpress 2011.1\IDETools\System\DXCore\BIN\DevExpress.CodeRush.Common.dll")
+
+echo 'OK'
+pause
